70bd147f77f3660b9b8596d8eedb1b2c4090d078
Lucas Mendonca <bragalucas@id.uff.br>
2021-11-30 21:52:24 +0000

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ServiceUtenteReference;
+using System;
+using System.Threading.Tasks;
+
+namespace DGSSOAPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = "1";
+            string[] contatos = { "2", "3" };
+            int addressId = 1;
+            int teamId = 1;
+            bool isolationStatus = true;
+            bool irregularity = true;
+            UtenteClient client = new UtenteClient();
+            try
+            {
+                var response = client.Set(codigo, contatos, addressId, teamId, isolationStatus, irregularity);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
